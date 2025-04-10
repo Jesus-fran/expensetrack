8a420117eeedf9a84792974df5e197244fbe0971
Jesus-fran <jesusfran.dev@gmail.com>
2025-04-10 17:04:02 -0600

    Tested db using pdo

--- public/index.php
+++ public/index.php
@@ -7,3 +7,13 @@ $dotenv->load();
 
 echo "Hello World! <br/>";
 
+$dsn = $_ENV['DB_DRIVER'] . ':dbname=' . $_ENV['DB_DATABASE'] . ';host=' . $_ENV['DB_HOST'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
+try {
+    $pdo = new PDO($dsn, $user, $pass);
+} catch (\PDOException $e) {
+    echo 'Failed: ' . $e->getMessage();
+}
+
+var_dump($pdo);
