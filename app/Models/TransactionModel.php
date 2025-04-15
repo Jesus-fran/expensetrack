a34f9fb1dffa8952c53d3285224e1731ec4f968d
Jesus-fran <jesusfran.dev@gmail.com>
2025-04-14 18:17:41 -0600

    Clean TransactionModel of functions with out use.

--- app/Models/TransactionModel.php
+++ app/Models/TransactionModel.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace App\Models;
 
 use App\Model;
-use DateTime;
-use stdClass;
 
 class TransactionModel extends Model
 {
@@ -24,22 +22,5 @@ class TransactionModel extends Model
         $transaction = $this->db->prepare('SELECT * FROM transactions');
         $transaction->execute();
         return $transaction->fetchAll(\PDO::FETCH_CLASS, '\App\Transaction');
-    }
-
-    public function ApplyFormat($id, $description, $check_num, $amount, $created_at, ): stdClass
-    {
-        return (object) [
-            'id' => $id,
-            'description' => $description,
-            'check_num' => $check_num,
-            'amount' => $this->formatMoney((float) $amount),
-            'created_at' => $this->formatCreatedAt($created_at),
-        ];
-    }
-
-    public function formatCreatedAt(string $created_at): string
-    {
-        $dateTime = new DateTime($created_at);
-        return $dateTime->format('M d, Y');
     }
 }
